b5cb8af69aaa914f7749ba90e263bec1fe3b3f0b
diogomarchi <diogo.marchi@hotmail.com>
2021-03-15 11:20:07 -0300

    finalizado a maquina de estados

--- maquinaEstados.vhd
+++ maquinaEstados.vhd
@@ -13,12 +13,15 @@ ENTITY maquinaEstados IS
   PORT (
     i_CLK   : IN std_logic; -- input clock
     i_CLR_n : IN std_logic; -- input clear/reset
-    i_MAIOR128 : IN std_logic; -- input larger or less that 128
-    o_ADDR  : OUT std_logic; -- output addr
-	 o_FINISH: out std_logic; -- output finish the Binarization
-    o_MAIOR128 : OUT std_logic; -- output larger or less that 128
-	 o_MAIOR4096: OUT STD_LOGIC; -- output larger or less that 4096
-    o_WR_EN_RAM: OUT std_logic -- output write enable in ram memory
+	 i_ADDR  : IN std_logic_vector(11 downto 0); --input addres
+    i_MAIOR128  : IN std_logic; -- input larger or less that 128
+	 i_MAIOR4096 : IN std_logic; -- input larger or less that 4096
+	 o_CLR       : out std_logic; --output clear
+    o_ADDR      : OUT std_logic_vector(11 downto 0); -- output addr
+    o_MAIOR128  : OUT std_logic; -- output verify if is larger 128
+	 o_MAIOR4096 : out std_logic; -- output verify if is larger 4096
+	 o_READY     : OUT STD_LOGIC; -- output larger or less that 4096
+    o_WR_EN_RAM : OUT std_logic -- output write enable in ram memory
   );
 END maquinaEstados;
 
@@ -39,7 +42,7 @@ ARCHITECTURE rtl OF maquinaEstados IS
 
 BEGIN
 
-  p_STATE : PROCESS (i_CLK, i_CLR_n, i_MAIOR128)
+  p_STATE : PROCESS (i_CLK, i_CLR_n, i_MAIOR128, i_MAIOR4096)
   BEGIN
     IF (i_CLR_n = '0') THEN
       r_STATE <= s_INIT; --initial state
@@ -55,30 +58,30 @@ BEGIN
         w_NEXT <= s_FINISH; --vai para o proximo passando clear em 1  
 
       WHEN s_FINISH =>
-        w_NEXT <= s_DECODE;
+        if(i_MAIOR4096 = '0') then
+		    w_NEXT <= s_LOAD;
+		  else 
+		    w_NEXT <= s_READY;
+		  end if;
 
-      WHEN s_DECODE =>
-        IF (w_OP = "0000") THEN -- caso op = load next state = carregar
-          w_NEXT <= s_LOAD;
-        ELSIF (w_OP = "0001") THEN -- caso op = armazenar next state = armazenar
-          w_NEXT <= s_STORE;
-        ELSIF (w_OP = "0010") THEN -- caso op = somar next state = somar
-          w_NEXT <= s_ADD;
-        ELSIF (w_OP = "0011") THEN -- caso op = carregaconst next state = carregaconst
-          w_NEXT <= s_LD_CONST;
-        ELSIF (w_OP = "0100") THEN -- caso op = sub A-B next state = sub A - B
-          w_NEXT <= s_SUB;
-        ELSIF (w_OP = "0101") THEN -- caso op = Jump next state = Jump
-          w_NEXT <= s_JUMPZ;
-        ELSIF (w_OP = "0110") THEN -- caso op = if RP < RQ 	0110 RW RP RQ
-          w_NEXT <= s_LT;
-        ELSIF (w_OP = "0111") THEN -- caso op = jump to 
-          w_NEXT <= s_JUMP;
-        ELSE
-          w_NEXT <= s_DECODE;
-        END IF;
-
-     
+      WHEN s_LOAD =>
+        w_NEXT <= s_VERIFY;
+		  
+		when s_VERIFY =>
+		  if(i_MAIOR128 = '1') then
+		    w_NEXT <= s_STORE1;
+		  else
+		    w_NEXT <= s_STORE0;
+		  end if;
+		
+		when s_STORE0 =>
+		  w_NEXT <= s_STORE_RAM;
+		  
+		when s_STORE1 =>
+		  w_NEXT <= s_STORE_RAM;
+		  
+		when s_STORE_RAM =>
+		  w_NEXT <= s_FINISH;  
 
       WHEN OTHERS =>
         w_NEXT <= s_INIT;
@@ -88,10 +91,17 @@ BEGIN
 
 
   --atribuição do valor de constante
-  o_MAIOR4096 <= '1' WHEN (r_STATE = s_FINISH) else '0';
+  
+  o_ADDR      <= i_ADDR when(r_STATE = s_LOAD) else "000000000000";     
+  
+  o_MAIOR128  <= '1' when(r_STATE = s_STORE1) else '0';
+  
+  o_MAIOR4096 <= '1' when(r_STATE = s_FINISH) else '0';
+  
+  o_READY     <= '1' WHEN (r_STATE = s_READY) else '0';
+  
+  o_WR_EN_RAM <= '1' when(r_STATE = s_STORE_RAM) else '0';
 
-  o_PC_CLR <= '1' WHEN (r_STATE = s_INIT) ELSE
-    '0';
-
+  o_CLR    <= '1' WHEN (r_STATE = s_INIT) ELSE '0';
   
 END rtl;
